4e3aceaf9f33850ba30a06922424b464e7ccfaa6
navicor90 <miltonivanterreno@gmail.com>
2020-06-08 18:01:31 -0300

    user validation

--- properties_core/views.py
+++ properties_core/views.py
@@ -1,8 +1,11 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from properties_core.serializers import UserSerializer, GroupSerializer
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 
+@method_decorator(login_required, name='dispatch')
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -11,6 +14,7 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
+@method_decorator(login_required, name='dispatch')
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
