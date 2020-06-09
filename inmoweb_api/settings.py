40f8f36972c101362683534288fc005899cf8575
navicor90 <miltonivanterreno@gmail.com>
2020-06-09 09:16:17 -0300

    collect static bug

--- inmoweb_api/settings.py
+++ inmoweb_api/settings.py
@@ -128,6 +128,8 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10
 }
 
+
+### MYSQL DATABASE CONNECTION
 try:
     # Check to make sure DATABASES is set in settings.py file.
     # If not default to {}
@@ -153,3 +155,18 @@ try:
             DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
 except Exception:
     print('Unexpected error:', sys.exc_info())
+
+
+# COLLECT STATIC HEROKU BUG
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
