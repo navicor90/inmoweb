c73bd24f326fd5fcd07c8fd2210c5b71b38861fb
navicor90 <miltonivanterreno@gmail.com>
2020-06-08 23:02:40 -0300

    auth checkers

--- properties_core/views.py
+++ properties_core/views.py
@@ -2,33 +2,32 @@ from django.contrib.auth.models import User, Group
 from properties_core.models import Property
 from rest_framework import viewsets
 from properties_core.serializers import UserSerializer, GroupSerializer, PropertySerializer
-from django.contrib.auth.decorators import login_required
-from django.utils.decorators import method_decorator
+from rest_framework import permissions
 
 
-@method_decorator(login_required, name='dispatch')
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
+    permission_classes = [permissions.IsAuthenticated]
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
 
-@method_decorator(login_required, name='dispatch')
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+    permission_classes = [permissions.IsAuthenticated]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 
-@method_decorator(login_required, name='dispatch')
 class PropertyViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
 
