c31f53bf4e071a0b83ae0ca35e6c45a0e2ce1293
navicor90 <miltonivanterreno@gmail.com>
2020-06-28 11:25:07 -0300

    validate previously created properties

--- properties_core/views.py
+++ properties_core/views.py
@@ -1,15 +1,18 @@
 from django.contrib.auth.models import User, Group
 from properties_core.models import Property
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from properties_core.serializers import UserSerializer, GroupSerializer, PropertySerializer
-from rest_framework import permissions
+from rest_framework.permissions import IsAuthenticated
+
+from rest_framework.response import Response
+import json
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]  
     queryset = User.objects.all().order_by('-date_joined')
     serializer_class = UserSerializer
 
@@ -18,16 +21,24 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
 
 class PropertyViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows groups to be viewed or edited.
+    API endpoint that allows properties to be viewed or edited.
     """
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [IsAuthenticated]
     queryset = Property.objects.all()
     serializer_class = PropertySerializer
 
+    def create(self, request):
+        data = json.loads(request.body.decode("utf-8"))
+        if 'ref_id' in data.keys() and 'source_web' in data.keys():
+            props = Property.objects.filter(ref_id=data['ref_id']) \
+                                    .filter(source_web=data['source_web'])
+            if len(props) > 0:
+                return Response(data={"reason": "Object already exists"}, status=status.HTTP_409_CONFLICT)
+        return super().create(request)
